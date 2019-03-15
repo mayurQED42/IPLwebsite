a572e196ce60d77ecbdd5b807f86c55a0d37677d
mayurQED42 <mayur.gajar@qed42.com>
2019-03-15 18:07:23 +0530

    writes gulp task

---
+++ web/themes/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+  return gulp.src('custom/mayur/sass/header.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('custom/mayur/css'));
+  //console.log('Hello mayur');
+  // place code for your default task here
+  //done();
+}
+
+gulp.task('sass', function() {
+  return gulp.src('custom/mayur/sass/header.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('custom/mayur/css'))
+   
+});
+
+gulp.task('watch', function() {
+  gulp.watch('custom/mayur/sass/header.scss', gulp.series('sass'));
+});
