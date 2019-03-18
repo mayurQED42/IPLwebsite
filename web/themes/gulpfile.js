3c6e36cce74b6029b70e60fc709ac0f4cec4e005
mayurQED42 <mayur.gajar@qed42.com>
2019-03-18 18:08:43 +0530

    writes task for compiled scss to css, compraised image, browser sync, watch tasks

--- web/themes/gulpfile.js
+++ web/themes/gulpfile.js
@@ -1,25 +1,40 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
+//task for compiled scss files to css.
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
   return gulp.src('custom/mayur/sass/header.scss') 
     .pipe(sass())
-    .pipe(gulp.dest('custom/mayur/css'));
+    .pipe(gulp.dest('custom/mayur/css'))
+    .pipe(browserSync.reload({stream: true}));
   //console.log('Hello mayur');
   // place code for your default task here
   //done();
 }
 
-gulp.task('sass', function() {
-  return gulp.src('custom/mayur/sass/header.scss') 
-    .pipe(sass())
-    .pipe(gulp.dest('custom/mayur/css'))
-   
+//task for compraises image size.
+gulp.task('images', function(){
+  return gulp.src('custom/mayur/images/**/*.+(png|jpg|gif|svg|jpeg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('custom/mayur/myimages'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch('custom/mayur/sass/header.scss', gulp.series('default'));
-  // watched tasks
+//task for browser sync.
+gulp.task('browserSync', function() {
+  browserSync.init({
+    open: 'external',
+    hostname: 'localhost',
+    port: 7777,
+    proxy: 'http://localhost:8887/8-2-19/MYIPL/web/'
+  })
 });
+
+// watched tasks
+gulp.task('watch', gulp.parallel('browserSync', function(done) {
+  gulp.watch('custom/mayur/sass/header.scss', gulp.series('default'));
+}));
+
